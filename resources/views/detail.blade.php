d41d1c62f732cd362ce591abf6bee8054f5f29fb
alvon17 <73384433+alvon17@users.noreply.github.com>
2022-12-16 18:20:21 +0700

    update image

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -7,7 +7,7 @@
 
         <div class="card flex-row mx-auto align-items-center justify-content-center shadow-none" style="border: none;">
             <div class="image-container">
-                <img class="image-left" src="{{ asset('image/Gerry.jpg') }}" width="400px">
+                <img class="image-left" src="{{ asset('image/Gerry.jpg') }}" width="400px" height="300px">
             </div>
             <div class="card-body">
                 <form>
