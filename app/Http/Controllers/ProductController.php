059f853a278f13587ffb4f0c0110e39d5b06af18
alvon17 <73384433+alvon17@users.noreply.github.com>
2022-12-17 17:32:31 +0700

    dynamic index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,17 +8,18 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function product () {
+    public function product()
+    {
         $products = Product::all();
         $categories = Category::all();
-        return view('index', ['products'=>$products, 'categories'=>$categories]);
+        return view('index', ['products' => $products, 'categories' => $categories]);
     }
 
-    public function category ($id) {
+    public function category($id)
+    {
         $categories = Category::all();
         $category = Category::find($id);
 
-        return view('category', ['category'=>$category, 'categories'=>$categories]);
+        return view('category', ['category' => $category, 'categories' => $categories]);
     }
-
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,60 +18,22 @@
             <a href="#">View All</a>
         </div>
         <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 container-body">
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('storage/dummy.webp') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
+            @foreach ($products as $product)
+                @php
+                    dd($product->category[0]->name);
+                @endphp
+                @if ($product->category[0]->name == 'Beauty')
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="{{ asset('storage/' . $product->photo) }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $product->name }}</h5>
+                                <p class="card-text">IDR {{ $product->price }}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
+                @endif
+            @endforeach
         </div>
     </div>
 
@@ -81,60 +43,19 @@
             <a href="#">View All</a>
         </div>
         <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 container-body">
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
+            @foreach ($products as $product)
+                @if ($product->category[1]->name == 'Camera')
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="{{ asset('storage/' . $product->photo) }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $product->name }}</h5>
+                                <p class="card-text">IDR {{ $product->price }}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="{{ asset('/image/gerry.jpg') }}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a short card.</p>
-                    </div>
-                </div>
-            </div>
+                @endif
+            @endforeach
         </div>
     </div>
 
