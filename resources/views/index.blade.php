4a6bc5930f9f86cfa9bd0238387d29e2b96c7a46
Chrylus <czar.soecahyo@binus.ac.id>
2022-12-17 18:01:34 +0700

    delete comment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -37,30 +37,4 @@
         </div>
     </div>
     @endforeach
-
-    <!-- <div class="fluid-container card-container mt-3">
-        <div class="row container-title d-flex align-items-center">
-            <h5>Camera</h5>
-            <a href="#">View All</a>
-        </div>
-        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 container-body">
-            @foreach ($products as $product)
-                @foreach ($product->category as $category)
-                    @if ($category->name == 'Camera')
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src="{{ asset('image/' . $product->photo) }}" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $product->name }}</h5>
-                                    <p class="card-text">IDR {{ $product->price }}</p>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
-                @endforeach
-            @endforeach
-        </div>
-    </div> -->
-
-
 @endsection
