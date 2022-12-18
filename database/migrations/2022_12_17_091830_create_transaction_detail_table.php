fa7ebaa43c9228307573857ea07f0af61749431e
alvon17 <73384433+alvon17@users.noreply.github.com>
2022-12-18 21:14:54 +0700

    delete seeder product + productcategory + change int to integer

--- database/migrations/2022_12_17_091830_create_transaction_detail_table.php
+++ database/migrations/2022_12_17_091830_create_transaction_detail_table.php
@@ -17,7 +17,7 @@ class CreateTransactionDetailTable extends Migration
             $table->id();
             $table->foreignId('transaction_id');
             $table->foreignId('product_id');
-            $table->int('quantity');
+            $table->integer('quantity');
             $table->timestamps();
 
             $table->foreign('transaction_id')->references('id')->on('transaction_header');
