e267bb4c06933b4aacd91d33de1319f097eccea3
alvon17 <73384433+alvon17@users.noreply.github.com>
2022-12-25 22:05:35 +0700

    Revert "fixing product name issue in history page"
    This reverts commit 8e956298f45b952d32567dccdfd7e28e302f4e32.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -65,14 +65,13 @@ class CartController extends Controller
         return redirect('cart');
     }
 
-    public function purchase()
-    {
+    public function purchase(){
         $cart = session('cart');
 
         $total_products = 0;
         $total_price = 0;
 
-        foreach ($cart as $c) {
+        foreach($cart as $c){
             $total_products += $c['quantity'];
             $total_price += $c['quantity'] * $c['price'];
         }
@@ -86,7 +85,7 @@ class CartController extends Controller
 
         $headerId = $transactionHeader->id;
 
-        foreach ($cart as $c) {
+        foreach($cart as $c){
             TransactionDetail::create([
                 'transaction_id' => $headerId,
                 'product_id' => $c['id'],
@@ -102,16 +101,13 @@ class CartController extends Controller
         return redirect('/dashboard');
     }
 
-    public function history()
-    {
+    public function history () {
         $categories = Category::all();
-        $transactionHeader = TransactionHeader::all();
-
+        $transactionHeader = TransactionHeader::all ();
+        
         $count = $transactionHeader->where('user_id', '=', auth()->user()->id)->pluck('id')->toArray();
         $transactionDetail = TransactionDetail::all()->whereIn('transaction_id', $count);
-        dd($transactionDetail);
-        $products = Product::all();
-
-        return view('transaction', ['categories' => $categories, 'trdetail' => $transactionDetail, 'count' => $count, 'transactionHeader' => $transactionHeader, 'products' => $products]);
+    
+        return view('transaction', ['categories' => $categories, 'trdetail' => $transactionDetail, 'count'=>$count, 'transactionHeader'=>$transactionHeader]);
     }
 }
--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -3,14 +3,14 @@
 @section('title', 'transaction')
 @section('content')
 
-    @for ($i = 0; $i < count($count); $i++)
+    @for ($i = 0; $i < count($count); $i ++)
         <div class="accordion accordion-container" id="accordionPanelsStayOpenExample">
             <div class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse"
                         data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
                         aria-controls="panelsStayOpen-collapseOne">
-                        Transaction Date {{ $transactionHeader[$i]->created_at }}
+                        Transaction Date {{$transactionHeader[$i]->created_at}}
                     </button>
                 </h2>
                 <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
@@ -29,43 +29,37 @@
                                 </th>
                             </tr>
 
-                            @foreach ($trdetail as $detail)
+                            @foreach($trdetail as $detail)
                                 @if ($detail->transaction_id == $transactionHeader[$i]->id)
                                     <tr>
                                         <td style="width: 60%;">
-                                            @foreach ($products as $product)
-                                                @if ($product->id == $detail->product_id)
-                                                    {{ $product->name }}
-                                                @break
-                                            @endif
-                                        @endforeach
+                                            {{$detail->product_id}}
+                                        </td>
+                                        <td>
+                                            {{$detail->quantity}}
+                                        </td>
+                                        <td>
+                                            {{$detail->sub_price}}
+                                        </td>
+                                    </tr>
+                                @endif
+                            @endforeach
 
-                                    </td>
-                                    <td>
-                                        {{ $detail->quantity }}
-                                    </td>
-                                    <td>
-                                        {{ $detail->sub_price }}
-                                    </td>
-                                </tr>
-                            @endif
-                        @endforeach
-
-                        <tr>
-                            <th>
-                                Total
-                            </th>
-                            <th>
-                                {{ $transactionHeader[$i]->total_products }}
-                            </th>
-                            <th>
-                                {{ $transactionHeader[$i]->total_price }}
-                            </th>
-                        </tr>
-                    </table>
+                            <tr>
+                                <th>
+                                    Total
+                                </th>
+                                <th>
+                                    {{$transactionHeader[$i]->total_products}}
+                                </th>
+                                <th>
+                                    {{$transactionHeader[$i]->total_price}}
+                                </th>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endfor
+    @endfor
 @endsection
