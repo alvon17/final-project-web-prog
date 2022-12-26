efa4eb5dc94c132652cc68877ab71c8411d1d53d
Chrylus <czar.soecahyo@binus.ac.id>
2022-12-26 15:42:23 +0700

    adding conditional if there is no history

--- resources/views/user/history.blade.php
+++ resources/views/user/history.blade.php
@@ -3,68 +3,74 @@
 @section('title', 'transaction')
 @section('content')
 
-    @foreach ($transactionHeader as $th)
-        <div class="accordion accordion-container" id="accordionPanelsStayOpenExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
-                        aria-controls="panelsStayOpen-collapseOne">
-                        Transaction Date {{ $th->created_at }}
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
-                    aria-labelledby="panelsStayOpen-headingOne">
-                    <div class="accordion-body">
-                        <table style="width: 100%;">
+    @if ($transactionHeader->count() == 0)
+        <div style="text-align: center;">
+            <h3>History is Empty</h3>
+        </div>
+    @else
+        @foreach ($transactionHeader as $th)
+            <div class="accordion accordion-container" id="accordionPanelsStayOpenExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                            aria-controls="panelsStayOpen-collapseOne">
+                            Transaction Date {{ $th->created_at }}
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
+                        aria-labelledby="panelsStayOpen-headingOne">
+                        <div class="accordion-body">
+                            <table style="width: 100%;">
+                                <tr>
+                                    <th>
+                                        Name
+                                    </th>
+                                    <th>
+                                        Quantity
+                                    </th>
+                                    <th>
+                                        Sub Price
+                                    </th>
+                                </tr>
+
+                                @foreach ($trdetail as $detail)
+                                    @if ($detail->transaction_id == $th->id)
+                                        <tr>
+                                            <td style="width: 60%;">
+                                                @foreach ($products as $product)
+                                                    @if ($product->id == $detail->product_id)
+                                                        {{ $product->name }}
+                                                    @break
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td>
+                                            {{ $detail->quantity }}
+                                        </td>
+                                        <td>
+                                            {{ $detail->sub_price }}
+                                        </td>
+                                    </tr>
+                                @endif
+                            @endforeach
+
                             <tr>
                                 <th>
-                                    Name
+                                    Total
                                 </th>
                                 <th>
-                                    Quantity
+                                    {{ $th->total_products }}
                                 </th>
                                 <th>
-                                    Sub Price
+                                    {{ $th->total_price }}
                                 </th>
                             </tr>
-
-                            @foreach ($trdetail as $detail)
-                                @if ($detail->transaction_id == $th->id)
-                                    <tr>
-                                        <td style="width: 60%;">
-                                            @foreach ($products as $product)
-                                                @if ($product->id == $detail->product_id)
-                                                    {{ $product->name }}
-                                                @break
-                                            @endif
-                                        @endforeach
-                                    </td>
-                                    <td>
-                                        {{ $detail->quantity }}
-                                    </td>
-                                    <td>
-                                        {{ $detail->sub_price }}
-                                    </td>
-                                </tr>
-                            @endif
-                        @endforeach
-
-                        <tr>
-                            <th>
-                                Total
-                            </th>
-                            <th>
-                                {{ $th->total_products }}
-                            </th>
-                            <th>
-                                {{ $th->total_price }}
-                            </th>
-                        </tr>
-                    </table>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endforeach
+        @endforeach
+    @endif
 @endsection
