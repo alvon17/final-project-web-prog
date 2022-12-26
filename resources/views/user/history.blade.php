3cc5d89a77a1ac51e11c9bda586378388c035f6a
Chrylus <czar.soecahyo@binus.ac.id>
2022-12-26 16:01:05 +0700

    fix history accordion open at the same time

--- resources/views/user/history.blade.php
+++ resources/views/user/history.blade.php
@@ -11,15 +11,15 @@
         @foreach ($transactionHeader as $th)
             <div class="accordion accordion-container" id="accordionPanelsStayOpenExample">
                 <div class="accordion-item">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                    <h2 class="accordion-header" id="panelsStayOpen-heading">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
-                            aria-controls="panelsStayOpen-collapseOne">
+                            data-bs-target="#panelsStayOpen-collapse{{$th->id}}" aria-expanded="true"
+                            aria-controls="panelsStayOpen-collapse{{$th->id}}">
                             Transaction Date {{ $th->created_at }}
                         </button>
                     </h2>
-                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
-                        aria-labelledby="panelsStayOpen-headingOne">
+                    <div id="panelsStayOpen-collapse{{$th->id}}" class="accordion-collapse collapse show"
+                        aria-labelledby="panelsStayOpen-heading{{$th->id}}">
                         <div class="accordion-body">
                             <table style="width: 100%;">
                                 <tr>
